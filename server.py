429471ee892e09947db0c96b96f8e3e051fcbe77
Sasha <alexalexbor04@gmail.com>
2024-05-26 16:00:58 +0300

    сервер добавлен

--- server.py
+++ server.py
@@ -1,15 +1,53 @@
-import socket
 import pickle
+import socket
 
-HOST = '127.0.0.1'
-PORT = 8080
+from algoritmizator import DiffHel, Coder
 
-sock = socket.socket()
-sock.bind((HOST, PORT))
-sock.listen(1)
-conn, addr = sock.accept()
+IP = '127.0.0.1'
+PORT = 12345
+ALLOWED_KEYS_FILE = "allowed_keys.txt"
+b = 123
+def load_allowed_keys():
+    with open(ALLOWED_KEYS_FILE, "r") as file:
+        return [int(line.strip()) for line in file]
 
-msg = conn.recv(1024)
-print(pickle.loads(msg))
+def main():
+    try:
+        sock = socket.socket()
+        sock.bind((IP, PORT))
+        sock.listen(1)
 
-conn.close()
+        coder = None
+        allowed_keys = load_allowed_keys()
+        conn, addr = sock.accept()
+
+        while True:
+            #на этом этапе "Hello, server!" не доходит до сервера
+            msg = conn.recv(4096)
+            data = pickle.loads(msg)
+
+            if type(data) == tuple:
+                p, g, A = data
+
+                if A not in allowed_keys:
+                    print("Незнакомый клиент. Подключение разорвано.")
+                    conn.close()
+                    continue
+
+                diff_hel = DiffHel(ab=b, p=p, g=g)
+                B = diff_hel.calculate_key
+                K = diff_hel.calculate_k(A)
+                coder = Coder(K)
+                conn.send(pickle.dumps(B))
+
+            else:
+                decrypted_message = coder.decode(data.decode())
+                print("Полученное сообщение = ", decrypted_message)
+
+                encrypted_answer = coder.code(decrypted_message)
+                conn.send(encrypted_answer.encode())
+    except EOFError:
+        sock.close()
+
+if __name__ == "__main__":
+    main()

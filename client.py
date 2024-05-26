ae924228ba67d55f81d74f29fc6ee807f16b818b
Sasha <alexalexbor04@gmail.com>
2024-05-26 15:58:15 +0300

    я дура

--- client.py
+++ client.py
@@ -1,41 +1,14 @@
-import pickle
 import socket
+import pickle
 
-from algoritmizator import DiffHel, Coder
+HOST = '127.0.0.1'
+PORT = 8080
 
-IP = '127.0.0.1'
-PORT = 12345
+sock = socket.socket()
+sock.connect((HOST, PORT))
 
-def main():
-    sock = socket.socket()
-    sock.connect((IP, PORT))
+p, g, a = 7, 5, 3
+A = g ** a % p
+sock.send(pickle.dumps((p, g, A)))
 
-    p = 54
-    g = 34
-    a = 43
-
-    diff_hel = DiffHel(ab=a, p=p, g=g)
-    A = diff_hel.calculate_key
-
-    with open("client_public_key.txt", "w") as file:
-        file.write(str(A))
-
-    sock.send(pickle.dumps((p, g, A)))
-
-    B = pickle.loads(sock.recv(4096))
-    K = diff_hel.calculate_k(B)
-
-    coder = Coder(K)
-
-    message = input("Введите сообщение: ")
-    encrypted_message = coder.code(message)
-    sock.send(pickle.dumps(encrypted_message.encode()))
-
-    answer = sock.recv(4096).decode()
-    decrypted_answer = coder.decode(answer)
-    print("Ответ от сервера = ", decrypted_answer)
-
-    sock.close()
-
-if __name__ == "__main__":
-    main()
+sock.close()

1d782f1a85016d230193994997159949e2d849ed
Sasha <alexalexbor04@gmail.com>
2024-05-26 15:53:17 +0300

    недоделанный клиент

--- client.py
+++ client.py
@@ -1,14 +1,41 @@
-import socket
 import pickle
+import socket
 
-HOST = '127.0.0.1'
-PORT = 8080
+from algoritmizator import DiffHel, Coder
 
-sock = socket.socket()
-sock.connect((HOST, PORT))
+IP = '127.0.0.1'
+PORT = 12345
 
-p, g, a = 7, 5, 3
-A = g ** a % p
-sock.send(pickle.dumps((p, g, A)))
+def main():
+    sock = socket.socket()
+    sock.connect((IP, PORT))
 
-sock.close()
+    p = 54
+    g = 34
+    a = 43
+
+    diff_hel = DiffHel(ab=a, p=p, g=g)
+    A = diff_hel.calculate_key
+
+    with open("client_public_key.txt", "w") as file:
+        file.write(str(A))
+
+    sock.send(pickle.dumps((p, g, A)))
+
+    B = pickle.loads(sock.recv(4096))
+    K = diff_hel.calculate_k(B)
+
+    coder = Coder(K)
+
+    message = input("Введите сообщение: ")
+    encrypted_message = coder.code(message)
+    sock.send(pickle.dumps(encrypted_message.encode()))
+
+    answer = sock.recv(4096).decode()
+    decrypted_answer = coder.decode(answer)
+    print("Ответ от сервера = ", decrypted_answer)
+
+    sock.close()
+
+if __name__ == "__main__":
+    main()
